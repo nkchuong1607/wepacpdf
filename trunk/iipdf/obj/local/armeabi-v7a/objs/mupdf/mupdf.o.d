498d46a87a1d065a0590fd79cce4b4f3fde771dd
nkchuong1607 <nkchuong1607@ce73a491-fbea-80f4-a89e-e2c2849d146e>
2014-02-20 09:08:55 +0000

    import libs, not fix config

---
+++ trunk/iipdf/obj/local/armeabi-v7a/objs/mupdf/mupdf.o.d
@@ -0,0 +1,393 @@
+obj/local/armeabi-v7a/objs/mupdf/mupdf.o: jni/mupdf.c \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/jni.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/time.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/time.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/time.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/siginfo.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/pthread.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/signal.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/limits.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/limits.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/limits.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/limits.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/page.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/signal.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/signal.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/sigcontext.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sched.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/log.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/bitmap.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdlib.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/alloca.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/strings.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/memory.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/math.h \
+ jni/../mupdf/fitz.h jni/../mupdf/../mupdf/fitz/system.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/assert.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/errno.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/errno.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/errno.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/fcntl.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/fcntl.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/fcntl.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/fcntl.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/unistd.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/select.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysconf.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/capability.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/pathconf.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/setjmp.h \
+ /cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/setjmp.h \
+ jni/../mupdf/memento.h jni/../mupdf/../mupdf/fitz/context.h \
+ jni/../mupdf/fitz/system.h jni/../mupdf/../mupdf/fitz/crypt.h \
+ jni/../mupdf/../mupdf/fitz/getopt.h jni/../mupdf/../mupdf/fitz/hash.h \
+ jni/../mupdf/fitz/context.h jni/../mupdf/../mupdf/fitz/math.h \
+ jni/../mupdf/../mupdf/fitz/string.h jni/../mupdf/../mupdf/fitz/xml.h \
+ jni/../mupdf/../mupdf/fitz/buffer.h jni/../mupdf/../mupdf/fitz/stream.h \
+ jni/../mupdf/fitz/buffer.h \
+ jni/../mupdf/../mupdf/fitz/compressed-buffer.h \
+ jni/../mupdf/fitz/stream.h jni/../mupdf/../mupdf/fitz/filter.h \
+ jni/../mupdf/../mupdf/fitz/output.h jni/../mupdf/../mupdf/fitz/store.h \
+ jni/../mupdf/../mupdf/fitz/colorspace.h jni/../mupdf/fitz/store.h \
+ jni/../mupdf/../mupdf/fitz/pixmap.h jni/../mupdf/fitz/math.h \
+ jni/../mupdf/fitz/colorspace.h jni/../mupdf/../mupdf/fitz/bitmap.h \
+ jni/../mupdf/fitz/pixmap.h jni/../mupdf/../mupdf/fitz/image.h \
+ jni/../mupdf/fitz/compressed-buffer.h \
+ jni/../mupdf/../mupdf/fitz/function.h jni/../mupdf/../mupdf/fitz/shade.h \
+ jni/../mupdf/../mupdf/fitz/font.h jni/../mupdf/../mupdf/fitz/path.h \
+ jni/../mupdf/../mupdf/fitz/text.h jni/../mupdf/fitz/font.h \
+ jni/../mupdf/fitz/path.h jni/../mupdf/../mupdf/fitz/device.h \
+ jni/../mupdf/fitz/image.h jni/../mupdf/fitz/shade.h \
+ jni/../mupdf/fitz/text.h jni/../mupdf/../mupdf/fitz/display-list.h \
+ jni/../mupdf/fitz/device.h jni/../mupdf/../mupdf/fitz/structured-text.h \
+ jni/../mupdf/fitz/output.h jni/../mupdf/../mupdf/fitz/transition.h \
+ jni/../mupdf/../mupdf/fitz/glyph-cache.h \
+ jni/../mupdf/../mupdf/fitz/link.h jni/../mupdf/../mupdf/fitz/outline.h \
+ jni/../mupdf/fitz/link.h jni/../mupdf/../mupdf/fitz/document.h \
+ jni/../mupdf/fitz/transition.h jni/../mupdf/fitz/outline.h \
+ jni/../mupdf/../mupdf/fitz/annotation.h jni/../mupdf/fitz/document.h \
+ jni/../mupdf/../mupdf/fitz/meta.h \
+ jni/../mupdf/../mupdf/fitz/write-document.h \
+ jni/../mupdf/../mupdf/fitz/output-pnm.h jni/../mupdf/fitz/bitmap.h \
+ jni/../mupdf/../mupdf/fitz/output-png.h \
+ jni/../mupdf/../mupdf/fitz/output-pwg.h \
+ jni/../mupdf/../mupdf/fitz/output-pcl.h \
+ jni/../mupdf/../mupdf/fitz/output-svg.h jni/../mupdf/pdf.h \
+ jni/../mupdf/../mupdf/fitz.h jni/../mupdf/../mupdf/pdf/object.h \
+ jni/../mupdf/../mupdf/pdf/document.h jni/../mupdf/../mupdf/pdf/parse.h \
+ jni/../mupdf/../mupdf/pdf/xref.h jni/../mupdf/../mupdf/pdf/crypt.h \
+ jni/../mupdf/../mupdf/pdf/page.h jni/../mupdf/../mupdf/pdf/resource.h \
+ jni/../mupdf/../mupdf/pdf/cmap.h jni/../mupdf/../mupdf/pdf/font.h \
+ jni/../mupdf/../mupdf/pdf/annot.h jni/../mupdf/../mupdf/pdf/field.h \
+ jni/../mupdf/../mupdf/pdf/widget.h \
+ jni/../mupdf/../mupdf/pdf/appearance.h jni/../mupdf/../mupdf/pdf/event.h \
+ jni/../mupdf/../mupdf/pdf/javascript.h \
+ jni/../mupdf/../mupdf/pdf/output-pdf.h
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/jni.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/cdefs_elf.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/api-level.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/time.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/time.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/types.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdint.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_types.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/_types.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/_wchar_limits.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/posix_types.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/stddef.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/compiler.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/posix_types.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/types.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/types.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/kernel.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysmacros.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/time.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/siginfo.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/siginfo.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/pthread.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/signal.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/limits.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/limits.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/limits.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/internal_types.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/limits.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/syslimits.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/page.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/string.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/malloc.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/signal.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/signal.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/sigcontext.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sched.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/log.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/android/bitmap.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdio.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/stdlib.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/alloca.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/strings.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/memory.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/math.h:
+
+jni/../mupdf/fitz.h:
+
+jni/../mupdf/../mupdf/fitz/system.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/assert.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/errno.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/errno.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/errno.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/errno.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/errno-base.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/fcntl.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/fcntl.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm/fcntl.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/asm-generic/fcntl.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/unistd.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/select.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/sys/sysconf.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/linux/capability.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/pathconf.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/setjmp.h:
+
+/cygdrive/d/android-ndk-r9b/platforms/android-8/arch-arm/usr/include/machine/setjmp.h:
+
+jni/../mupdf/memento.h:
+
+jni/../mupdf/../mupdf/fitz/context.h:
+
+jni/../mupdf/fitz/system.h:
+
+jni/../mupdf/../mupdf/fitz/crypt.h:
+
+jni/../mupdf/../mupdf/fitz/getopt.h:
+
+jni/../mupdf/../mupdf/fitz/hash.h:
+
+jni/../mupdf/fitz/context.h:
+
+jni/../mupdf/../mupdf/fitz/math.h:
+
+jni/../mupdf/../mupdf/fitz/string.h:
+
+jni/../mupdf/../mupdf/fitz/xml.h:
+
+jni/../mupdf/../mupdf/fitz/buffer.h:
+
+jni/../mupdf/../mupdf/fitz/stream.h:
+
+jni/../mupdf/fitz/buffer.h:
+
+jni/../mupdf/../mupdf/fitz/compressed-buffer.h:
+
+jni/../mupdf/fitz/stream.h:
+
+jni/../mupdf/../mupdf/fitz/filter.h:
+
+jni/../mupdf/../mupdf/fitz/output.h:
+
+jni/../mupdf/../mupdf/fitz/store.h:
+
+jni/../mupdf/../mupdf/fitz/colorspace.h:
+
+jni/../mupdf/fitz/store.h:
+
+jni/../mupdf/../mupdf/fitz/pixmap.h:
+
+jni/../mupdf/fitz/math.h:
+
+jni/../mupdf/fitz/colorspace.h:
+
+jni/../mupdf/../mupdf/fitz/bitmap.h:
+
+jni/../mupdf/fitz/pixmap.h:
+
+jni/../mupdf/../mupdf/fitz/image.h:
+
+jni/../mupdf/fitz/compressed-buffer.h:
+
+jni/../mupdf/../mupdf/fitz/function.h:
+
+jni/../mupdf/../mupdf/fitz/shade.h:
+
+jni/../mupdf/../mupdf/fitz/font.h:
+
+jni/../mupdf/../mupdf/fitz/path.h:
+
+jni/../mupdf/../mupdf/fitz/text.h:
+
+jni/../mupdf/fitz/font.h:
+
+jni/../mupdf/fitz/path.h:
+
+jni/../mupdf/../mupdf/fitz/device.h:
+
+jni/../mupdf/fitz/image.h:
+
+jni/../mupdf/fitz/shade.h:
+
+jni/../mupdf/fitz/text.h:
+
+jni/../mupdf/../mupdf/fitz/display-list.h:
+
+jni/../mupdf/fitz/device.h:
+
+jni/../mupdf/../mupdf/fitz/structured-text.h:
+
+jni/../mupdf/fitz/output.h:
+
+jni/../mupdf/../mupdf/fitz/transition.h:
+
+jni/../mupdf/../mupdf/fitz/glyph-cache.h:
+
+jni/../mupdf/../mupdf/fitz/link.h:
+
+jni/../mupdf/../mupdf/fitz/outline.h:
+
+jni/../mupdf/fitz/link.h:
+
+jni/../mupdf/../mupdf/fitz/document.h:
+
+jni/../mupdf/fitz/transition.h:
+
+jni/../mupdf/fitz/outline.h:
+
+jni/../mupdf/../mupdf/fitz/annotation.h:
+
+jni/../mupdf/fitz/document.h:
+
+jni/../mupdf/../mupdf/fitz/meta.h:
+
+jni/../mupdf/../mupdf/fitz/write-document.h:
+
+jni/../mupdf/../mupdf/fitz/output-pnm.h:
+
+jni/../mupdf/fitz/bitmap.h:
+
+jni/../mupdf/../mupdf/fitz/output-png.h:
+
+jni/../mupdf/../mupdf/fitz/output-pwg.h:
+
+jni/../mupdf/../mupdf/fitz/output-pcl.h:
+
+jni/../mupdf/../mupdf/fitz/output-svg.h:
+
+jni/../mupdf/pdf.h:
+
+jni/../mupdf/../mupdf/fitz.h:
+
+jni/../mupdf/../mupdf/pdf/object.h:
+
+jni/../mupdf/../mupdf/pdf/document.h:
+
+jni/../mupdf/../mupdf/pdf/parse.h:
+
+jni/../mupdf/../mupdf/pdf/xref.h:
+
+jni/../mupdf/../mupdf/pdf/crypt.h:
+
+jni/../mupdf/../mupdf/pdf/page.h:
+
+jni/../mupdf/../mupdf/pdf/resource.h:
+
+jni/../mupdf/../mupdf/pdf/cmap.h:
+
+jni/../mupdf/../mupdf/pdf/font.h:
+
+jni/../mupdf/../mupdf/pdf/annot.h:
+
+jni/../mupdf/../mupdf/pdf/field.h:
+
+jni/../mupdf/../mupdf/pdf/widget.h:
+
+jni/../mupdf/../mupdf/pdf/appearance.h:
+
+jni/../mupdf/../mupdf/pdf/event.h:
+
+jni/../mupdf/../mupdf/pdf/javascript.h:
+
+jni/../mupdf/../mupdf/pdf/output-pdf.h:
